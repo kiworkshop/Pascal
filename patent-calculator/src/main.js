a13beb6efb007a318eb6838de01a4ad5b372744b
Kwangil Cho <kwangil.steve.cho@gmail.com>
2018-09-22 16:09:37 +0900

    [INITIAL] BE DBMS로서 firebase의 firestore를 채택하였습니다.

--- patent-calculator/src/main.js
+++ patent-calculator/src/main.js
@@ -2,8 +2,20 @@ import '@babel/polyfill'
 import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCdmCxOB54j8Gq4_qQ-ZmRVMwhhtlivvhY",
+  authDomain: "trademark-quotation.firebaseapp.com",
+  databaseURL: "https://trademark-quotation.firebaseio.com",
+  projectId: "trademark-quotation",
+  storageBucket: "trademark-quotation.appspot.com",
+  messagingSenderId: "632762029401"
+};
+firebase.initializeApp(config);
 
 new Vue({
   render: h => h(App)
