e36dba44f6b74eb9a1fba558826db50dc5fa41ae
syngchoi <syngchoi@naver.com>
2018-09-25 20:29:10 +0900

    [ADD] FE 데이터 테이블을 구현하였습니다.
    1. 데이터 테이블 구현 완료
      - 테스트를 위해 작은 데이터(3000개)를 사용합니다.
    
    TODO
      - 검색하기) 데이터 테이블에서 추가버튼을 눌렀을 때 내 상표 가방으로
        보내져야 합니다.
      - 검색하기) 선택된 상품 목록을 표시해 주어야 합니다.

--- patent-calculator/src/components/Search.vue
+++ patent-calculator/src/components/Search.vue
@@ -1,8 +1,61 @@
 <template>
-  <v-container fluid>
+  <v-container fluid grid-list-md>
     <v-slide-y-transition mode="out-in">
-      <v-layout column align-center>
-        검색하기
+      <v-layout row wrap>
+        <v-flex xs8>
+          <v-flex class="h_title">검색하기</v-flex>
+          <v-flex class="h_explanation">
+            <p>특허청 고시상품명칭 11판(2018)에서 상품 명칭을 검색합니다.</p>
+          </v-flex>
+          <v-card>
+            <v-layout>  
+              <v-spacer></v-spacer>
+              <v-flex>
+              <v-select
+                v-model="search.category"
+                :items="categories"
+                label="분류"
+              ></v-select> <!--분류별 검색 기능 추가-->
+              </v-flex>
+              <v-spacer></v-spacer>
+              <v-flex>
+              <v-text-field
+                v-model="search.name"
+                append-icon="search"
+                label="명칭"
+                single-line
+                hide-details
+              ></v-text-field>
+              </v-flex>
+              <v-spacer></v-spacer>
+            </v-layout>
+            <v-data-table
+              :headers="headers"
+              :items="products"
+              :search="search.name"
+              :rows-per-page-items="rowsPerPageItems"
+            >
+              <template slot="items" slot-scope="props">
+                <td class="text-xs-center">{{ props.item['NICE분류'] }}</td>
+                <td> {{ props.item['지정상품(국문)'] }}</td>
+                <td> {{ props.item['지정상품(영문)'] }}</td>
+                <td class="text-xs-center">{{ props.item['유사군코드'] }}</td>
+                <td class="text-xs-center">
+                  <!--추가하기-->
+                  <v-btn flat icon slot="activator" color="primary" dark @click.native="add_to_list(props.item)">
+                    <v-icon small>add</v-icon>
+                  </v-btn>            
+                </td>
+              </template>
+              <v-alert slot="no-results" :value="true" color="error" icon="warning">
+                Your search for "{{ search.name }}" found no results.
+              </v-alert>
+            </v-data-table>
+          </v-card>
+        </v-flex>
+        <!-- TODO: 현재 담은 목록을 간단하게 보여줍니다. -->
+        <v-flex xs4>
+        </v-flex>
       </v-layout>
     </v-slide-y-transition>
   </v-container>
@@ -10,13 +63,68 @@
 
 <script>
 export default {
-  name: 'Search',
+  name: "Search",
+  data() {
+    return {
+      rowsPerPageItems: [
+        10,
+        25,
+        { text: "$vuetify.dataIterator.rowsPerPageAll", value: -1 }
+      ],
+      search: {
+        category: "",
+        name: ""
+      },
+      headers: [
+        {
+          text: "분류",
+          align: "center",
+          value: "NICE분류"
+        },
+        {
+          text: "명칭",
+          align: "left",
+          sortable: false,
+          value: "지정상품(국문)"
+        },
+        {
+          text: "영문 명칭",
+          align: "left",
+          sortable: false,
+          value: "지정상품(영문)"
+        },
+        {
+          text: "유사군 코드",
+          align: "center",
+          sortable: false,
+          value: "유사군코드"
+        },
+        {
+          text: "추가하기",
+          align: "center",
+          sortable: false,
+          value: "추가하기"
+        }
+      ]
+    };
+  },
+  methods: {
+    add_to_list(item) {
+      alert("지정상품에 추가되었습니다.");
+      // TODO
+      // 1. alert창을 snackbar로 교체
+      // 2. 선택된 아이템을 vuex store에 저장합니다.
+    }
+  },
   computed: {
-    products () {
-      return this.$store.getters.products
+    categories() {
+      return this.$store.getters.categories;
+    },
+    products() {
+      return this.$store.getters.products;
     }
   }
-}
+};
 </script>
 
 <style scoped>
--- patent-calculator/src/vuex/store.js
+++ patent-calculator/src/vuex/store.js
@@ -7,7 +7,63 @@ const UPDATE_PRODUCTS = 'UPDATE_PRODUCTS'
 Vue.use(Vuex);
 export const store = new Vuex.Store({
     state: {
-        products: {}
+        products: {},
+        categories: [
+            '전체',
+            '1류 (공업용, 과학용 및 농업용 화학품)',
+            '2류 (페인트, 착색제, 부식방지제, 목재보존재)',
+            '3류 (세정제 및 화장용품 제제)',
+            '4류 (공업용 오일, 연료, 발광체)',
+            '5류 (약제)',
+            '6류 (금속제품, 금속제, 건축재료)',
+            '7류 (기계, 기계공구, 모터 및 엔진)',
+            '8류 (수동식 기기)',
+            '9류 (전기기기, 과학기기)',
+            '10류 (의료기기)',
+            '11류 (환경제어장치)',
+            '12류 (수송기계기구)',
+            '13류 (화기 및 화공품)',
+            '14류 (귀금속 제품, 보석류[장신구], 시계류)',
+            '15류 (악기)',
+            '16류 (종이제품, 인쇄물, 문방구)',
+            '17류 (고무제품, 반가공 플라스틱, 절연, 단열 방음용 재료)',
+            '18류 (가죽제품, 마구)',
+            '19류 (비금속제 건축재료)',
+            '20류 (가구, 플라스틱 제품)',
+            '21류 (가정용 및 주방용 기구, 화장용구, 유리 및 도자기제품',
+            '22류 (로프, 끈, 직물용 미가공 섬유)',
+            '23류 (직물용 실)',
+            '24류 (직물, 가정용 직물제 커버)',
+            '25류 (의류, 신발, 모자)',
+            '26류 (의류 및 헤어 장식품)',
+            '27류 (완공된 마루 및 벽 등에 붙이는 비품)',
+            '28류 (오락 및 놀이용구, 운동용품)',
+            '29류 (육류, 가공식품)',
+            '30류 (곡물, 커피, 차, 코코아)',
+            '31류 (신선한(가공하지 않은) 육산물, 살아있는 동식물 및 사료)',
+            '32류 (비알코올성 음료, 맥주)',
+            '33류 (알코올 음료)',
+            '34류 (담배, 흡연용품, 성냥)',
+            '35류 (광고, 기업관리업)',
+            '36류 (금융업, 보험업, 부동산업)',
+            '37류 (건설업, 수선업)',
+            '38류 (통신업)',
+            '39류 (운송[사람, 동물, 물품] 서비스업, 물품 보관서비스업)',
+            '40류 (재료처리업[주문자 제조방식의 서비스 포함])',
+            '41류 (교육업, 연예오락업)',
+            '42류 (컴퓨터, 과학기술 서비스업)',
+            '43류 (음식료품 제공서비스업, 임시숙박업)',
+            '44류 (의료, 미용, 농업)',
+            '45류 (개인 또는 법무 서비스업)'
+        ]
+    },
+    getters: {
+        products (state) {
+            return state.products
+        },
+        categories (state) {
+            return state.categories
+        }
     },
     mutations: {
         [UPDATE_PRODUCTS] (state, products) {
@@ -16,7 +72,7 @@ export const store = new Vuex.Store({
     },
     actions: {
         fetchProducts ({commit}) {
-            const dataUrl = "https://s3.ap-northeast-2.amazonaws.com/pascal.kiworkshop/data/classification"
+            const dataUrl = "https://s3.ap-northeast-2.amazonaws.com/pascal.kiworkshop/data/classification(minified)"
             axios.get(dataUrl)
             .then(result => result.data)
             .then(products => commit(UPDATE_PRODUCTS, products))
