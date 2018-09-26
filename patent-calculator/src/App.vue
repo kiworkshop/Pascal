1dfe595c3256f0857bc1fd5d6058a755e7ddca8c
Kwangil Cho <kwangil.steve.cho@gmail.com>
2018-09-26 14:50:46 +0900

    [UPDATE] 메뉴를 drawer에서 toolbar 형태로 변경하였습니다.
    1. 내 상표가방에 현재 선택된 상품들의 갯수를 badge로 표현하였습니다.
    2. 카테고리 데이터를 네트워크를 통해 받아오는 형식으로 바꾸었습니다.

--- patent-calculator/src/App.vue
+++ patent-calculator/src/App.vue
@@ -1,35 +1,15 @@
 <template>
   <v-app>
-    <v-navigation-drawer
-      persistent
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      enable-resize-watcher
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile
-          v-for="(item, i) in menu"
-          :key="i"
-          @click="view=item.component"
-        >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar
-      app
-      :clipped-left="clipped"
-    >
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+    <v-toolbar app>
       <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn flat v-for="(item, i) in menu" :key="i" @click="view=item.component">
+        <v-badge color="blue-grey lighten-3">
+          <span v-if="item.badge" slot="badge">{{selectedCount}}</span>
+          <v-icon>{{item.icon}}</v-icon>
+          {{item.title}}
+      </v-badge>
+      </v-btn>
     </v-toolbar>
     <v-content>
       <!-- 만약 투명도 변화를 주고 싶다면 css에 opacity transition 속성 추가 -->
@@ -59,31 +39,37 @@ export default {
   data() {
     return {
       title: "상표 견적 도우미",
-      clipped: false,
-      drawer: false,
       fixed: false,
       menu: [
         {
           icon: "search",
           title: "검색하기",
-          component: "Search"
+          component: "Search",
+          badge: false
         },
         {
           icon: "work",
           title: "내 상표 가방",
-          component: "Briefcase"
+          component: "Briefcase",
+          badge: true
         },
         {
           icon: "insert_chart_outlined",
           title: "견적내기",
-          component: "Quotation"
+          component: "Quotation",
+          badge: false
         }
       ],
       view: "Search",
-      miniVariant: false
     };
   },
+  computed: {
+    selectedCount () {
+      return this.$store.getters.selectedCount
+    }
+  },
   created () {
+    this.$store.dispatch('fetchCategories')
     this.$store.dispatch('fetchProducts')
   }
 };
