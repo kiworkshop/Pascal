43e52d288908d81cfaedadae262774994f81d19f
Kwangil Cho <kwangil.steve.cho@gmail.com>
2018-09-28 23:31:04 +0900

    [ADD] 작은 가방에 견적서 합계 UI를 추가하였습니다.

--- patent-calculator/src/components/BriefcaseSummary.vue
+++ patent-calculator/src/components/BriefcaseSummary.vue
@@ -1,41 +1,55 @@
 <template>
   <v-card>
-        <v-toolbar color="grey lighten-4">
-          <v-toolbar-title>선택된 상표들</v-toolbar-title>
-        </v-toolbar>
-      <v-list>
-          <v-list-group
-            v-for="(category, categoryNo) in selected"
-            v-if="Object.keys(category).length > 0"
-            :key="categoryNo"
-          >
-            <v-list-tile slot="activator">
-              <v-list-tile-content>
-                <v-list-tile-title>
-                  {{ categories[categoryNo] }}
-                </v-list-tile-title>
-              </v-list-tile-content>
-              <v-list-tile-action>
-                <v-avatar color="blue" size=22>
-                  <span class="white--text body-1 font-weight-medium">
-                    {{ Object.keys(category).length }}
-                  </span>
-                </v-avatar>
-              </v-list-tile-action>
-            </v-list-tile>
+    <v-toolbar color="grey lighten-4">
+      <v-toolbar-title>선택된 상표</v-toolbar-title>
+    </v-toolbar>
+    <v-list>
+      <v-list-group
+        v-for="(category, categoryNo) in selected"
+        v-if="Object.keys(category).length > 0"
+        :key="categoryNo"
+      >
+        <v-list-tile slot="activator">
+          <v-list-tile-content>
+            <v-list-tile-title>
+              {{ categories[categoryNo] }}
+            </v-list-tile-title>
+          </v-list-tile-content>
+          <v-list-tile-action>
+            <v-avatar color="blue" size=22>
+              <span class="white--text body-1 font-weight-medium">
+                {{ Object.keys(category).length }}
+              </span>
+            </v-avatar>
+          </v-list-tile-action>
+        </v-list-tile>
+        <v-list-tile v-for="(item, itemNo) in category" :key="itemNo">
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item['지정상품(국문)'] }}</v-list-tile-title>
+          </v-list-tile-content>
+          <v-icon color="red" small @click="deleteItem(item)">delete</v-icon>
+        </v-list-tile>
+      </v-list-group>
+    </v-list>
+    <v-divider></v-divider>
+    <v-list two-line subheader>
+      <v-list-tile avatar>
+        <v-list-tile-avatar>
+          <v-icon>bar_chart</v-icon>
+        </v-list-tile-avatar>
 
-            <v-list-tile
-                v-for="(item, itemNo) in category"
-                :key="itemNo"
-            >
-              <v-list-tile-content>
-                <v-list-tile-title>{{ item['지정상품(국문)'] }}</v-list-tile-title>
-              </v-list-tile-content>
-              <!-- <v-icon color="red" small class="mr-2" @click="editItem(item)">edit</v-icon> -->
-              <v-icon color="red" small @click="deleteItem(item)">delete</v-icon>
-            </v-list-tile>
-          </v-list-group>
-        </v-list>
+        <v-list-tile-content>
+          <v-list-tile-sub-title>합계</v-list-tile-sub-title>
+          <v-list-tile-title>1,000,000 원</v-list-tile-title>
+        </v-list-tile-content>
+
+        <v-list-tile-action>
+          <v-btn icon ripple>
+            <v-icon color="grey lighten-1">info</v-icon>
+          </v-btn>
+        </v-list-tile-action>
+      </v-list-tile>
+    </v-list>
   </v-card>
 </template>
 
--- patent-calculator/src/components/Search.vue
+++ patent-calculator/src/components/Search.vue
@@ -3,10 +3,10 @@
     <v-slide-y-transition mode="out-in">
       <v-layout row wrap>
         <v-flex xs8>
-          <v-flex class="h_title">검색하기</v-flex>
+          <!-- <v-flex class="h_title">검색하기</v-flex>
           <v-flex class="h_explanation">
             <p>특허청 고시상품명칭 11판(2018)에서 상품 명칭을 검색합니다.</p>
-          </v-flex>
+          </v-flex> -->
           <v-card>
             <v-layout>  
               <v-spacer></v-spacer>
