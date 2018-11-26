196202bf1b0edc418a5ee695bb5997ceae569982
deocksoo <dusol92@snu.ac.kr>
2018-11-26 15:29:41 +0900

    [EDIT] 상품 추가기능을 수정합니다
    상품 추가기능의 Step2에 divider를 추가하였고, 비고시상품 목록에서
    분류를 지정할 수 있도록 하는 select bar와 버튼을 추가하였습니다.
    분류 지정 기능 추후에 더 업데이트 될 예정입니다.

--- patent-calculator/src/components/ProductAdder/ClassifiedResult.vue
+++ patent-calculator/src/components/ProductAdder/ClassifiedResult.vue
@@ -1,39 +1,7 @@
 <template>
   <v-container>
-    <!-- <v-layout column wrap> -->
-      <!-- <v-flex class='mb-5'>
-        <v-layout row>
-          <v-flex xs4>
-            <v-select
-              v-model="payload._class"
-              :items="classes"
-              label="분류"
-            ></v-select>
-          </v-flex>
-          <v-spacer></v-spacer>
-          <v-flex xs7>
-            <v-textarea
-              v-model="payload.searchingProducts"
-              append-icon="search"
-              placeholder="명칭 (붕산비료, 생물 비료, 도매업)"
-              auto-grow
-              hint="특허청 고시상품명칭 11판(2018)에서 상품 명칭을 검색합니다."
-              persistent-hint
-              rows="1"
-            ></v-textarea>
-          </v-flex>
-        </v-layout>
-      </v-flex>
-
-      <v-flex xs1 offset-xs10 class="mb-5">
-        <v-btn color="primary" @click="classifyProducts()">
-          상품 분류
-        </v-btn>
-      </v-flex> -->
-
-      <!-- <v-flex> -->
         <v-layout row wrap>
-          <v-flex xs5>
+          <v-flex xs5 class='mr-5'>
             <v-data-table
               :headers="noticedProductsHeaders"
               :items="products.noticed"
@@ -57,42 +25,64 @@
             </v-data-table>
           </v-flex>
 
+          <v-divider vertical></v-divider>
+
           <v-flex xs6 class="ml-5">
-            <v-data-table
-              :headers="unnoticedProductsHeaders"
-              :items="products.unnoticed"
-              :rows-per-page-items="rowsPerPageItems"
-              no-data-text="검색 결과가 없습니다."
-              class="mt-3"
-            >
-              <template slot="items" slot-scope="props">
-                <tr :active="props.selected">
-                  <td>
-                    <v-checkbox
-                    :input-value="props.selected"
-                    primary
-                    hide-details
-                    @click="props.selected = !props.selected"
-                    ></v-checkbox>
-                  </td>
-                  <td class="text-xs-center">{{ props.item['NICE분류'] }}</td>
-                  <td class="text-xs-center"><input type="text" v-model="props.item['지정상품(국문)']"></td>
-                  <td class="text-xs-center">
-                    <!--추가하기-->
-                    <v-btn flat icon slot="activator" color="primary" dark @click.native="moveProduct(props.item.id)">
-                      <v-icon small>compare_arrows</v-icon>
-                    </v-btn>
-                    <v-btn flat icon slot="activator" color="red" dark @click.native="deleteFromList(props.item.id)">
-                      <v-icon small>delete</v-icon>
-                    </v-btn>
-                  </td>
-                </tr>
-              </template>
-            </v-data-table>
+            <v-layout column>
+              <v-flex>
+                <v-data-table
+                  :headers="unnoticedProductsHeaders"
+                  :items="products.unnoticed"
+                  :rows-per-page-items="rowsPerPageItems"
+                  no-data-text="검색 결과가 없습니다."
+                  class="mt-3"
+                >
+                  <template slot="items" slot-scope="props">
+                    <tr :active="props.selected">
+                      <td>
+                        <v-checkbox
+                        :input-value="props.selected"
+                        primary
+                        hide-details
+                        @click="props.selected = !props.selected"
+                        ></v-checkbox>
+                      </td>
+                      <td class="text-xs-center">{{ props.item['NICE분류'] }}</td>
+                      <td class="text-xs-center"><input type="text" v-model="props.item['지정상품(국문)']"></td>
+                      <td class="text-xs-center">
+                        <!--추가하기-->
+                        <v-btn flat icon slot="activator" color="primary" dark @click.native="moveProduct(props.item.id)">
+                          <v-icon small>compare_arrows</v-icon>
+                        </v-btn>
+                        <v-btn flat icon slot="activator" color="red" dark @click.native="deleteFromList(props.item.id)">
+                          <v-icon small>delete</v-icon>
+                        </v-btn>
+                      </td>
+                    </tr>
+                  </template>
+                </v-data-table>
+              </v-flex>
+              <v-flex>
+                <v-layout row>
+                  <v-spacer></v-spacer>
+                    <v-flex>
+                      <v-select
+                      v-model="payload._class"
+                      :items="classes"
+                      label="분류"
+                      ></v-select> <!--적용할 분류 검색 기능-->
+                    </v-flex>
+                    <v-spacer></v-spacer>
+                    <v-flex class="pt-2">
+                      <v-btn color="white" @click="applyClass()">선택한 항목에 분류 적용</v-btn>
+                    </v-flex>
+                    <v-spacer></v-spacer>
+                </v-layout>
+              </v-flex>
+
+            </v-layout>
           </v-flex>
         </v-layout>
-      <!-- </v-flex> -->
-    <!-- </v-layout> -->
   </v-container>
 </template>
 
@@ -101,6 +91,7 @@ export default {
   name: "ClassifiedResult",
   data() {
     return {
+      classes: [1, 2, 3],
       rowsPerPageItems: [10, 25, 100],
       // TODO: _class의 기본값을 설정해주어야합니다.
       payload: {
