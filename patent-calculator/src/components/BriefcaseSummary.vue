7da324116cc68549e1b0e096fc38620bc81d2263
Kwangil Cho <kwangil.steve.cho@gmail.com>
2018-12-02 14:23:42 +0900

    [ADD] 요약 사이드바의 선택된 상품에서 분류 리스트 중 하나를 클릭 시 내용이 클립보드에 복사됩니다.
    this commit resolves #66

--- patent-calculator/src/components/BriefcaseSummary.vue
+++ patent-calculator/src/components/BriefcaseSummary.vue
@@ -11,21 +11,29 @@
         v-for="(products, classNo) in selected"
         v-if="Object.keys(products).length > 0"
         :key="classNo"
+        @click="copyToClipboard(classNo, products)"
       >
-        <v-list-tile-content>
-          {{ classes[classNo] }}
-        </v-list-tile-content>
+        <v-list-tile-content>{{ classes[classNo] }}</v-list-tile-content>
         <v-list-tile-avatar>
-          <v-avatar slot="activator" color="primary" size=22>
-            <span class="white--text body-1 font-weight-medium">
-              {{ Object.keys(products).length }}
-            </span>
+          <v-avatar slot="activator" color="primary" size="22">
+            <span class="white--text body-1 font-weight-medium">{{ Object.keys(products).length }}</span>
           </v-avatar>
         </v-list-tile-avatar>
       </v-list-tile>
       <v-list-tile v-if="Object.keys(selected).length === 0">
         <v-list-tile-content>선택된 상품이 없습니다.</v-list-tile-content>
       </v-list-tile>
+      <template v-else>
+        <v-divider></v-divider>
+        <v-list-tile>
+          <v-list-tile-content>
+            <v-list-tile-title class="font-weight-bold">총 {{Object.keys(selected).length}}개 류</v-list-tile-title>
+          </v-list-tile-content>
+          <v-list-tile-action>
+            <v-flex class="text-xs-right font-weight-bold">{{selectedCount}}개 지정상품</v-flex>
+          </v-list-tile-action>
+        </v-list-tile>
+      </template>
     </v-list>
   </v-card>
 </template>
@@ -33,13 +41,27 @@
 <script>
 export default {
   name: "BriefcaseSummary",
+  methods: {
+    copyToClipboard(classNo, products) {
+      products = Object.values(products).reduce((acc, val) => {
+        acc = acc.concat(val['지정상품(국문)'])
+        return acc;
+      }, []).join(", ");
+      navigator.clipboard.writeText(products);
+      const message = this.classes[classNo] + "의 지정상품이 클립보드에 복사되었습니다.";
+      this.$noticeEventBus.$emit("raiseNotice", message);
+    }
+  },
   computed: {
     selected() {
       return this.$store.getters.selected;
     },
+    selectedCount() {
+      return this.$store.getters.selectedCount;
+    },
     classes() {
       return Object.values(this.$store.getters.classes);
-    },
+    }
   }
 };
 </script>
